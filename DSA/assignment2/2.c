50a2ae55a8be3a78bfb665439fe3243408a29da5
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-01-25 21:39:07 +0530

    Update 2.c

--- DSA/assignment2/2.c
+++ DSA/assignment2/2.c
@@ -1,11 +1,4 @@
-/*Implement a menu driven Program in C for the following operations on Doubly Linked List (DLL) of Employee Data with the fields: emp ID , Name, Dept, Designation, Sal, PhNo 
-a. Create a DLL of N Employees Data by using end insertion. 
-b. Display the status of DLL and count the number of nodes in it 
-c. Perform Insertion and Deletion at End of DLL 
-d. Perform Insertion and Deletion at Front of DLL 
-e. Exit
-*/
-
+/* I have used dynamic memory allocation in this question also and used scanf for input for strings*/
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -14,6 +7,7 @@ typedef struct node {
     char *name;
     char *dept;
     char *designation;
+    char *phn;
     long long int salary;  //employee sal may be very high, that's why I'm using long long int.
     struct node* next;
     struct node* prev;
@@ -21,10 +15,10 @@ typedef struct node {
 
 
 //function declaration
-Empl *insertion_at_front(Empl* start,int id,char *name,char* dept,char* designation, long long int salary);
+Empl *insertion_at_front(Empl* start,int id,char *name,char* dept,char* designation, long long int salary,char *phn);
 Empl  *creation(Empl* start);
 void count_print(Empl* start);
-Empl* insertion_at_end(Empl* start,int id,char *name,char* dept,char* designation, long long int salary);
+Empl* insertion_at_end(Empl* start,int id,char *name,char* dept,char* designation, long long int salary,char *phn);
 Empl *deletion(Empl* start);
 
 
@@ -36,7 +30,7 @@ int main(){
     printf("1. Create a DLL of N Employees Data by using end insertion \n2. Display and Count the total number of Employees \n3. Insertion at end \n4. Deletion at end\n5. Exit");
     printf("\n********************\n");
     int opt;
-    Node *start = NULL;
+    Empl *start = NULL;
     while(1){
         scanf("%d",&opt);
         switch(opt){
@@ -60,10 +54,11 @@ int main(){
                 //insertion at end
                 int id;
                 long long int salary;
-                char *name,*dept,*designation;
+                char *name,*dept,*designation,*phn;
                 name = (char*)malloc(sizeof(char)*50);
                 designation = (char*)malloc(sizeof(char)*50);
-                dept = (char*)malloc(sizeof(char)*100);
+                dept = (char*)malloc(sizeof(char)*50);
+                phn = (char*)malloc(sizeof(char)*11);
                 printf("Enter Employee's Name\n");
                 scanf(" %[^\n]",name);
                 printf("Enter Employee's designation\n");
@@ -71,12 +66,15 @@ int main(){
                 scanf(" %[^\n]",designation);
                 printf("Enter Employee's department\n");
                 fflush(stdout);
-                scanf(" %[^\n]",department);
+                scanf(" %[^\n]",dept);
+                printf("Enter Employee's Contact Number\n");
+                fflush(stdout);
+                scanf(" %[^\n]",phn);
                 printf("Enter the Employee's salary ");
                 scanf(" %lld",&salary);
                 printf("Enter the Employee's id ");
                 scanf(" %d",&id);
-                start = insertion_at_end(start,id,name,dept,designation,salary);
+                start = insertion_at_end(start,id,name,dept,designation,salary,phn);
                 printf("Insertion completed");
                 printf("\n********************\n");
                 break;
@@ -119,7 +117,7 @@ void count_print(Empl* start){
     }
     // traversing throught the DLL
     while(p !=NULL){
-        printf("Id:  %d  Name :  %s   Department  %s   Designation %s  Salary %lld \n",p->id,p->name,p->dept,p->designation,p->salary);
+        printf("Id:  %d  Name :  %s   Department  %s   Designation %s  Salary %lld Contact Number %s\n",p->id,p->name,p->dept,p->designation,p->salary,p->phn);
         p = p->next;
         count++;
     }
@@ -130,6 +128,122 @@ void count_print(Empl* start){
 
 
 
+//insertion at front of DLL
 
+Empl* insertion_at_front(Empl* start,int id,char *name,char* dept,char* designation, long long int salary,char* phn){
+    Empl*tmp =(Empl*)malloc(sizeof(Empl));
+    if(tmp == NULL){
+        //memory not available
+        printf("memory cannot be allocated\n");
+        return start;
+    }
+    tmp->id = id;
+    tmp->name= name;
+    tmp->dept =dept;
+    tmp->designation = designation;
+    tmp->salary = salary;
+    tmp->phn = phn;
+    
+    if(start == NULL) { // DLL is empty
+        start = tmp;
+        tmp->next = NULL;
+        tmp->prev = NULL;
+        return start;
+    }
+    
+    tmp->next = start;
+    tmp->prev = NULL;
+    start->prev = tmp;
+    start = tmp;
+    return start;
+}
+
+
+//insertion at end of DLL
+Empl* insertion_at_end(Empl* start,int id,char *name,char* dept,char* designation, long long int salary,char* phn){
+    Empl* tmp,*p;
+    tmp =(Empl*)malloc(sizeof(Empl));
+    if(tmp == NULL){
+        //memory not available
+        printf("memory cannot be allocated\n");
+        return start;
+    }
+    
+    tmp->id = id;
+    tmp->name= name;
+    tmp->dept =dept;
+    tmp->designation = designation;
+    tmp->salary = salary;
+    tmp->phn = phn;
+    
+    p = start;
+    while(p->next != NULL){
+        p = p->next;
+    }
+    tmp->next = p->next;
+    tmp->prev = p;
+    p->next = tmp;
+    return start;
+}
+
+
+
+// Deletion at end of DLL
+Empl *deletion(Empl* start){
+    Empl *tmp = start;
+    if(tmp == NULL){   // DLL is already empty
+        printf("List is already empty\n");
+        return start;
+    }
+    if(start->next == NULL) {  // only one node is present in the SLL
+        start = NULL;
+        free(tmp);
+        return start;
+    }
+    
+    while(tmp->next->next != NULL){   // traversing to the second last node.
+        tmp = tmp->next;
+    }
+    
+    free(tmp->next);
+    tmp->next = NULL;
+    return start;
+}
+
+
+// creation function
+Empl* creation(Empl* start){
+    printf("Enter the number of Employees\n");
+    int n;
+    scanf("%d",&n);
+    
+    while(n!=0){
+        int id;
+        long long int salary;
+        char *name,*dept,*designation,*phn;
+        name = (char*)malloc(sizeof(char)*50);
+        designation = (char*)malloc(sizeof(char)*50);
+        dept = (char*)malloc(sizeof(char)*50);
+        phn = (char*)malloc(sizeof(char)*11);
+        printf("Enter Employee's Name\n");
+        scanf(" %[^\n]",name);
+        printf("Enter Employee's designation\n");
+        fflush(stdout);
+        scanf(" %[^\n]",designation);
+        printf("Enter Employee's department\n");
+        fflush(stdout);
+        scanf(" %[^\n]",dept);
+        printf("Enter Employee's Contact number\n");
+        fflush(stdout);
+        scanf(" %[^\n]",phn);
+        printf("Enter the Employee's salary ");
+        scanf(" %lld",&salary);
+        printf("Enter the Employee's id ");
+        scanf(" %d",&id);        
+        start =  insertion_at_front(start,id,name,dept,designation,salary,phn);    //using  insertion_at_front() function to create the DLL.
+        n--;
+    }
+    return start;
+}
 
 
