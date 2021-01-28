8ba1a82502d8d2eecb4e79737718e0df794b3df9
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-01-29 02:55:28 +0530

    Update Main.java

--- OOPJ/Module2/Main.java
+++ OOPJ/Module2/Main.java
@@ -1,6 +1,8 @@
 package project0;
 import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 class User{
 	String name;
@@ -17,13 +19,104 @@ class User{
 
 
 class Hotel{
-	File users; // File which stores the password of different users for login
+	File users;// File which stores the password of different users for login
+	User currUser;
+	int idAssigned = 0;   // each user will be assigned a different id
+	private int loggedin = -1; // to check if current user is logged in or not.
+	Hotel(){
+		try {
+			this.users = new File("C:\\Users\\sresh\\Desktop\\Hotel\\users.txt");
+			users.createNewFile();
+		}
+		catch(IOException e) {
+			System.out.println("Error occured !");
+		}
+	}
 	
+	public void signUp(User user) {
+		try {
+			Scanner scanner = new Scanner(System.in);
+			String pass,cpass;
+			FileOutputStream fo = new FileOutputStream(this.users,true);
+			System.out.println("Please enter your password");
+			pass = scanner.next();
+			System.out.println("Please confirm your password");
+			cpass = scanner.next();
+			if(cpass.equals(pass)) {
+				user.password = pass;
+				user.id  = this.idAssigned;
+				String s = user.name +","+user.password+","+this.idAssigned+","+user.age+"\n";  // storing in comma seperated format
+				byte b[] = s.getBytes();
+				fo.write(b);
+				this.idAssigned += 1;
+				System.out.println("Your id is "+this.idAssigned);  
+				System.out.println("Account created ! Please login to continue");
+				fo.close();
+				return;
+			}
+			else {
+				System.out.println("Password does not match !");
+				signUp(user);
+				return;
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Account cannot be created. Try again later !");
+			return;
+		}
+		
+	}
+	
+	
+	public void login() {
+		try {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name (username)");
+		String name = sc.next();
+		System.out.println("Enter your password");
+		String pass = sc.next();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(this.users)));
+		
+		String currLine;
+		String fname,fpass;
+		while((currLine = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(currLine,",");
+			if((fname = st.nextToken()).equals(name)) {
+				if((fpass = st.nextToken()).equals(pass)) {
+					//successfully logged in
+					this.loggedin = 1;
+					System.out.println("Logged in !");
+					return;
+				}
+				else {
+					System.out.println("Password does not match !");
+					return;
+				}
+			}
+			else {
+				st.nextToken();
+				st.nextToken();
+				continue;
+			}
+		}
+		}
+		catch(IOException e) {
+			System.out.println("Cannot login ! Try again later.");
+			return;
+		}
+	}
+	
+	
+	public void logOut() {
+		this.loggedin = -1;
+		System.out.println("Logged out");
+		return;
+	}
 }
 
 class Main{
-	Hotel hotel = new Hotel();
-	public static void main(String args[]) {
+	public static void main(String args[]){
+		Hotel hotel = new Hotel();
 		System.out.println("Choose from the following");
 		System.out.println("1. Login ");
 		System.out.println("2. Signup");
@@ -36,21 +129,26 @@ class Main{
 				
 			case 1:{
 				//login
-				
-				
+				hotel.login();
 				System.out.println("************");
 				break;
 			}
 			
 			case 2:{
 				//sign  up
-				
+				System.out.println("Enter your name (username)");
+				String name = sc.next();
+				System.out.println("Enter your age");
+				int age = sc.nextInt();
+				User user = new User(name, age, hotel.idAssigned);
+				hotel.signUp(user);
 				System.out.println("************");
 				break;
 			}
 			
 			case 3: {
 				//exit
+				hotel.logOut();
 				System.exit(0);
 				
 			}
