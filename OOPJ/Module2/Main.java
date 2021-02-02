304f0234e01780fb6345d3951a7cc6bcff417acf
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-02-02 21:55:58 +0530

    Update Main.java

--- OOPJ/Module2/Main.java
+++ OOPJ/Module2/Main.java
@@ -62,7 +62,8 @@ class Hotel{
 	List<Traveller> travellers;
 	String city;   //city in which the hotel is located.
 	int rooms[];  
-	Hotel(String name, String city) {
+	final int price;  // charge for one night stay
+	Hotel(String name, String city,int price) {
 		this.name = name;
 		this.city = city;
 		this.travellers = new ArrayList<Traveller>();
@@ -70,6 +71,7 @@ class Hotel{
 		for(int i =0;i<15;i++) {
 			this.rooms[i] = 0;    // initially all rooms will be vacant.
 		}
+		this.price = price;
 	}
 }
 
@@ -84,15 +86,15 @@ class HotelBooking extends CheckDate{
 	int idAssigned = 0;   // each user will be assigned a different id
 	private int loggedin = -1; // to check if current user is logged in or not.
 	HotelBooking(){
-		hotels.add(new Hotel("Taj","New Delhi"));
-		hotels.add(new Hotel("Paradise","Varanasi"));
-		hotels.add(new Hotel("The Imperial Hotel","Jaipur"));
-		hotels.add(new Hotel("Elsewhere","Goa"));
-		hotels.add(new Hotel("The Leela Palace","Bengaluru"));
-		hotels.add(new Hotel("Surya Samudra","Kovalam"));
-		hotels.add(new Hotel("Taj Lake Palace","Udaipur"));
-		hotels.add(new Hotel("Umaid Bhawan Palace","Jodhpur"));
-		hotels.add(new Hotel("Lotus House Boat","Kerela"));
+		hotels.add(new Hotel("Taj","New Delhi",1000));
+		hotels.add(new Hotel("Paradise","Varanasi",2000));
+		hotels.add(new Hotel("The Imperial Hotel","Jaipur",3000));
+		hotels.add(new Hotel("Elsewhere","Goa",600));
+		hotels.add(new Hotel("The Leela Palace","Bengaluru",800));
+		hotels.add(new Hotel("Surya Samudra","Kovalam",900));
+		hotels.add(new Hotel("Taj Lake Palace","Udaipur",1200));
+		hotels.add(new Hotel("Umaid Bhawan Palace","Jodhpur",2200));
+		hotels.add(new Hotel("Lotus House Boat","Kerela",3400));
 		
 		
 		try {
@@ -286,12 +288,21 @@ class HotelBooking extends CheckDate{
 				
 				//writing check in and check out date in file userBookings
 				try {
+					Scanner sc1 = new Scanner(System.in);
 					FileOutputStream fo =new FileOutputStream(this.userBookings,true);
-					String write = hotel.name + "," + currUser.id+","+numDays+","+","+checkinDate+","+checkoutDate;
+					String write = currUser.id+","+hotel.price + "," +numDays+","+","+","+","+checkinDate+","+checkoutDate;
 					byte b[] = write.getBytes();
 					fo.write(b);
 					hotel.rooms[i] = 1;
 					System.out.println("Your room is booked.");
+					System.out.println("Are you a traveller? 1/0");
+					int opt = sc1.nextInt();
+					if(opt == 1) {
+						hotel.travellers.add((Traveller) currUser);
+					}
+					else {
+						continue;
+					}
 					return;
 				}
 				catch(IOException e) {
@@ -309,9 +320,39 @@ class HotelBooking extends CheckDate{
 		try {
 			
 			//calculating bill from userBookings file and writing bill in userBills file
-			FileInputStream fr = new FileInputStream(userBookings);
-			FileOutputStream fo = new FileOutputStream(userBills,true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(this.userBookings)));
+			FileOutputStream fo = new FileOutputStream(this.userBills,true);
+			String currLine;
+			int bill = 0;
+			int price = 0,numDays = 0;
+			while((currLine = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(currLine,",");
+				
+				//Finding user by Id
+				if(currUser.id == Integer.parseInt(st.nextToken())) {
+					numDays = Integer.parseInt(st.nextToken());
+					price = Integer.parseInt(st.nextToken());
+					bill = numDays*price;
+					break;
+				}
+				else {
+					continue;
+				}
+				
+			}
 			
+			//if bill is greater than 10000, discount of 1000 Rs is granted
+			if(bill>=10000) {
+				bill = bill-1000;
+			}
+			
+			System.out.println("Your bill is Rs "+bill);
+			
+			//writing data in userBills file
+			String s = currUser.id+","+(bill);
+			byte b[] = s.getBytes();
+			fo.write(b);
+			fo.close();
 		}
 		catch(IOException e) {
 			System.out.println("Cannot calculate bill. Try again later !");
