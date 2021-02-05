ae256a3e18d7614116971a55b46d70a372a2f299
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-02-05 10:39:55 +0530

    Create Main.java

---
+++ OOPJ/Main.java
@@ -0,0 +1,292 @@
+package project0;
+
+import java.util.Date;
+import java.util.Scanner;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+class Employee{
+	int id;
+	String name;
+	Date dob;
+	Department department;
+	Employee(int id,String name,Date dob,Department department){
+		this.id = id;
+		this.name = name;
+		this.dob =dob;
+		this.department = department;
+	}
+}
+
+class Department{
+	int id;
+	String location;
+	Department(int id,String location){
+		this.id = id;
+		this.location = location;
+	}
+}
+
+class Manager extends Employee{
+
+	Manager(int id, String name, Date dob, Department department) {
+		super(id, name, dob, department);
+		
+	}
+	
+}
+
+class Clerk extends Employee{
+
+	Clerk(int id, String name, Date dob, Department department) {
+		super(id, name, dob, department);
+		
+	}
+	
+}
+
+
+class GDW extends Employee{
+
+	GDW(int id, String name, Date dob, Department department) {
+		super(id, name, dob, department);
+		
+	}
+	
+}
+
+
+
+class Sales extends Department{
+
+	Sales(int id, String location) {
+		super(id, location);
+		
+	}
+	
+}
+
+class Marketing extends Department{
+
+	Marketing(int id, String location) {
+		super(id, location);
+		
+	}
+	
+}
+
+
+class Finance extends Department{
+	Finance(int id, String location) {
+		super(id, location);
+		
+	}
+	
+}
+
+
+
+//created a class Admin
+class Admin{
+	Employee e;
+	File adminFile;
+	Admin(Employee e){
+		this.e = e;
+	}
+	
+	public void setEmployeeData() {
+		this.adminFile = new File("C:\\Users\\sresh\\Desktop\\OOPJ\\admin.txt");
+		try {
+			this.adminFile.createNewFile();
+		}
+		catch(IOException e) {
+			System.out.println("Error occured");
+		}
+		
+	}
+}
+
+
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("Choose from the following");
+			System.out.println("1. Admin");
+			System.out.println("2. User");
+			System.out.println("3. Exit");
+			System.out.println("***********");
+			int opt = sc.nextInt();
+			switch(opt) {
+			case 1:{
+				
+				
+				//admin
+				Admin admin = null;
+				Employee e = null;
+				Department d = null;
+				String name;
+				int id;
+				Date dob = new Date();
+				int dId;
+				String dCity;
+				while(true) {
+					System.out.println("Choose from the following");
+					System.out.println("1. Enter Employee Data");
+					System.out.println("2. Exit");
+					System.out.println("**************");
+					int opt2 = sc.nextInt();
+					switch(opt2) {
+					case 1:{
+						//enter the data
+						System.out.println("Choose from the following");
+						System.out.println("Enter Employee name");
+						name = sc.next();
+						
+						//throw error
+						System.out.println("Enter Employee id");
+						id = sc.nextInt();
+						//throw error
+						System.out.println("Enter Employee DOB");
+						String dateString = sc.next();
+						//throw error
+						SimpleDateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+						try {
+							dob = format.parse(dateString);
+						}
+						catch(ParseException exp) {
+							System.out.println("Wrong Format");
+						}
+						
+						//throw error
+						System.out.println("Enter department id");
+					    dId = sc.nextInt();
+					    //throw error
+						System.out.println("Enter department city");
+					    dCity = sc.next();
+					    //throw error
+						System.out.println("Choose the department ");
+						System.out.println("1. Sales");
+						System.out.println("2. Marketing");
+						System.out.println("3. Finance");
+						int depType = sc.nextInt();
+						switch(depType) {
+						case 1:{
+							d = new Sales(dId, dCity);
+							break;
+						}
+						
+						case 2:{
+							d = new Marketing(dId, dCity);
+							break;
+						}
+						
+						case 3:{
+							d = new Finance(dId, dCity);
+							break;
+						}
+						
+						default:{
+							System.out.println("Invalid Option");
+						}
+						}
+						
+						System.out.println("1. Manager");
+						System.out.println("2. Clerk");
+						System.out.println("3. GDW");
+						int empType = sc.nextInt();
+						switch(empType) {
+						case 1:{
+							e = new Manager(id, name, dob,d);
+							break;
+						}
+						
+						case 2:{
+							e = new Clerk(id, name, dob, d);
+							break;
+						}
+						
+						case 3:{
+							e = new Clerk(id, name, dob, d);
+							break;
+						}
+						
+						default:{
+							System.out.println("Invalid option");
+							
+						}
+						}
+						
+						admin = new Admin(e);
+						admin.setEmployeeData();
+						
+						break;
+					}
+					
+					case 2:{
+					
+						System.exit(0);
+					}
+					
+					default:{
+						System.out.println("Invalid Option");
+					}
+					}
+				}
+				
+				
+			}
+			
+			case 2:{
+				
+				
+				
+				//user
+				
+				while(true) {
+					System.out.println("Choose from the following");
+					System.out.println("1. Display User Data");
+					System.out.println("2. Exit");
+					int opt3 = sc.nextInt();
+					switch(opt3) {
+					case 1:{
+						
+						// display user data by name and id
+						System.out.println("Display");
+						break;
+					}
+					
+					case 2:{
+						
+						// exit
+						
+						System.exit(0);
+					}
+					
+					default:{
+						System.out.println("Invalid option");
+					}
+					}
+				}
+				
+				
+			}
+			
+			
+			case 3:{
+				
+				System.exit(0);
+			}
+			
+			default:{
+				System.out.println("Invalid option");
+			}
+			
+			
+			}
+		}
+	}
+}
